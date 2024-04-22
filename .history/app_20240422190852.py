eeb443f61cb9d17f66ccb758b305fea0b566ce5f
25042 <2504225565@qq.com>
2024-04-22 19:10:00 +0800

    fix bugs

--- .history/app_20240422190852.py
+++ .history/app_20240422190852.py
@@ -6,7 +6,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 # download internlm2 to the base_path directory using git tool
 base_path = './internlm2-chat-1.8b'
 #internLM-chat-1.8B_lora.git
-os.system(f'git clone https://code.openxlab.org.cn/wly23/internLM-chat-1.8B_lora.git {base_path}')
+os.system(f'git clone https://code.openxlab.org.cn/wly23internLM-chat-1.8B_lora.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
